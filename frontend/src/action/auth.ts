d62ff818570d60fc4c017c626e008eedea4c3ad2
Priyanshu Thapliyal <114170980+Priyanshuthapliyal2005@users.noreply.github.com>
2025-09-04 12:30:09 +0530

    authentication done

---
+++ frontend/src/action/auth.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import { hashPassword } from "~/lib/auth";
+import { signupSchema, type SignupFormValues } from "~/schemas/auth";
+import { db } from "~/server/db";
+import Stripe from "stripe";
+
+type SignupResult = {
+    success: boolean;
+    error?: string;
+}
+
+export async function signUp(data: SignupFormValues): Promise<SignupResult> {
+    const validationResult = signupSchema.safeParse(data);
+    if(!validationResult.success){
+        return {
+            success: false,
+            error: validationResult.error.issues[0]?.message || "Invalid Input",
+        };
+    }
+    
+    const {email, password} = validationResult.data;
+
+    try {
+        const existingUser = await db.user.findUnique({where: {email}});
+        
+        if(existingUser){
+            return {
+                success: false,
+                error: "Email already in use",
+            };
+        }
+
+        const hashedPassword = await hashPassword(password);
+        
+        // const stripe = new Stripe("Todo: Stripe Key");
+
+        // const stripeCustomer = await stripe.customers.create({
+        //     email: email.toLocaleLowerCase(),
+        // })
+
+        const newUser = await db.user.create({
+            data: {
+                email,
+                password: hashedPassword,
+                // stripeCustomerId: stripeCustomer.id
+            },
+        });
+        return {
+            success: true,
+        };
+    } catch(error){
+        return {
+            success: false,
+            error: "An error occur during signup."
+        }
+    }
+}
